0da358d409f0e49daa79ead83b8ba5c5292c1f05
thunderbiscuit <thunderB@protonmail.com>
2020-05-14 17:38:34 -0400

    add fully functional website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,37 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import "../styles/styles.css"
+
+class LandingPage extends React.Component {
+  queryAPI() {
+    console.log("fetching latest Kraken USD price...")
+    fetch("https://api.kraken.com/0/public/Ticker?pair=XXBTZUSD")
+      .then(response => response.json())
+      .then(data => {
+        const price = data.result.XXBTZUSD.c[0]
+        const pricerounded = Math.floor(price)
+        console.log("latest price: ", pricerounded)
+        document.getElementById("current-price").innerText = "$ " + pricerounded
+      })
+  }
+
+  componentDidMount() {
+    // initial API query
+    this.queryAPI()
+
+    // query API every 5 seconds
+    setInterval(() => {
+      this.queryAPI()
+    }, 5000)
+  }
+
+  render() {
+    return (
+      <div className={"main"}>
+        <p id={"current-price"} className={"btc-ticker"}></p>
+      </div>
+    )
+  }
+}
+
+export default LandingPage
