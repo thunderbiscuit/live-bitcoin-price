4f3f8c7e75b5267700e99176d93f79946fddbf99
thunderbiscuit <thunderB@protonmail.com>
2020-05-14 22:41:47 -0400

    improve 404 and easter egg

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+
 import "../styles/styles.css"
 
 function FourOFour() {
   return (
-    <div className={"flex-column"}>
-      <h1 className={"btc-ticker"}>404.</h1>
-      <h3 className={"subtitle"}>
-        How... how did you get here? This is a 1-page website!
-      </h3>
-      <Link to="/" className={"subtitle"}>
-        Take me home
-      </Link>
-      <Link to="/easter-egg" className={"sub-subtitle"}>
-        Take me... somewhere else.
-      </Link>
-    </div>
+    <Layout>
+      <div className={"flex-column"}>
+        <h1 className={"btc-ticker"}>404.</h1>
+        <h3 className={"subtitle"}>
+          <span className="fade-in fade-0">How... </span>
+          <span className="fade-in fade-1">how did you get here? </span>
+          <span className="fade-in fade-2">This is a 1-page website!</span>
+          <br />
+          <span className="fade-in fade-3">
+            How could you <em>possibly</em> get so lost?
+          </span>
+        </h3>
+        <Link to="/" className={"subtitle"}>
+          Take me home
+        </Link>
+        <Link to="/easter-egg" className={"sub-subtitle"}>
+          Take me... somewhere else.
+        </Link>
+      </div>
+    </Layout>
   )
 }
 
--- src/pages/easter-egg.js
+++ src/pages/easter-egg.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/layout.js"
 import "../styles/styles.css"
 
 class Doge extends React.Component {
   queryAPI() {
     console.log("fetching latest Kraken USD price...")
     fetch("https://api.kraken.com/0/public/Ticker?pair=XDGUSD")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         const price = data.result.XDGUSD.c[0]
-        document.getElementById("current-easter-egg-price").innerText =
-          "$ " + price
+        if (document.getElementById("current-easter-egg-price") !== null) {
+          document.getElementById("current-easter-egg-price").innerText = "$ " + price
+        } else {
+          console.log("Easter egg is still fetching data.")
+        }
       })
   }
 
@@ -27,22 +31,26 @@ class Doge extends React.Component {
 
   render() {
     return (
-      <div className={"flex-column"}>
-        <figure>
-          <img src="/images/doge.png" alt="doge" />
-        </figure>
-        <p
-          id={"current-easter-egg-price"}
-          className={"btc-ticker"}
-          style={{ marginTop: "0em" }}
-        ></p>
-        <p className={"subtitle"} style={{ marginTop: "-2em" }}>
-          Well... you asked for it.
-        </p>
-        <Link to="/" className={"subtitle"}>
-          Now take me home.
-        </Link>
-      </div>
+      <>
+        <Layout>
+          <div className={"flex-column"}>
+            <figure>
+              <img src="/images/doge.png" alt="doge" />
+            </figure>
+            <p
+              id={"current-easter-egg-price"}
+              className={"btc-ticker"}
+              style={{ marginTop: "0em" }}
+            ></p>
+            <p className={"subtitle"} style={{ marginTop: "-2em" }}>
+              Well... you asked for it.
+            </p>
+            <Link to="/" className={"subtitle"}>
+              Now take me home.
+            </Link>
+          </div>
+        </Layout>
+      </>
     )
   }
 }
