e1f513fe23fd17e4e476c3fd88aeee4731aea6aa
thunderbiscuit <thunderB@protonmail.com>
2020-05-14 22:02:52 -0400

    add meta tags and twitter cards data

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,23 @@ const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
+        <title>Live Bitcoin Price</title>
+        <meta name="description" content="The simplest bitcoin price ticker on the internets!" />
+        <meta property="og:title" content="Bitcoin Price Ticker" />
+        <meta property="og:image" content="https://live-bitcoin-price/images/socialmedia.png" />
+        <meta property="og:url" content="https://live-bitcoin-price.com/" />
+        <meta
+          property="og:description"
+          content="The simplest bitcoin price ticker on the internets!"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="https://live-bitcoin-price.com/" />
+        <meta name="twitter:title" content="Bitcoin Price Ticker" />
+        <meta
+          name="twitter:description"
+          content="The simplest bitcoin price ticker on the internets!"
+        />
+        <meta name="twitter:image" content="https://live-bitcoin-price/images/socialmedia.png" />
         <script
           async
           defer
